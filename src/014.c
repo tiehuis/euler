6fdba0ebe41b5265e2b778b9f7636b6658fd0b93
Tiehuis <mti@protonmail.ch>
2014-09-24 19:28:01 +1200

    Added problem 14 sol

---
+++ src/014.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+#define CACHE_THRESH 1000000
+
+int main(void)
+{
+    int cache[CACHE_THRESH] = { 0 };
+    int maxstart;
+    int maxchain = 0;
+
+    int i;
+    for (i = 1; i < 1000000; ++i) {
+        unsigned long long a = i;
+        int chain = 1;
+
+        while (a > 1) {
+            if (a < CACHE_THRESH && cache[a]) {
+                chain += cache[a] - 1;
+                break;
+            }
+
+            if (a & 1)
+                a = a * 3 + 1;
+            else
+                a >>= 1;
+
+            ++chain;
+        }
+
+        if (i < CACHE_THRESH)
+            cache[i] = chain;
+
+        if (chain > maxchain) {
+            maxstart = i;
+            maxchain = chain;
+        }
+    }
+
+    printf("%d\n", maxstart);
+    return 0;
+}
