d16a2501bba404a0cac76ddba43629e66fb4ac93
Tiehuis <mti@protonmail.ch>
2014-09-25 22:09:20 +1200

    Added problem 18 sol

--- src/005.c
+++ src/005.c
@@ -3,12 +3,14 @@
 
 int main(void)
 {
-    long val[] = { 20, 19, 18, 17, 16, 15, 14, 13, 12, 11 };
+    const long val[] = { 20, 19, 18, 17, 16, 15, 14, 13, 12, 11 };
+    const long size = sizeof(val) / sizeof(val[0]);
+
     long i = 1;
     long k = val[0];
     long d = val[0];
 
-    for (i = 1; i < (long)arrsiz(val); ++i) {
+    for (i = 1; i < size; ++i) {
         while (k % val[i])
             k += d;
         d = lcm(d, val[i]);
--- src/func/common.h
+++ src/func/common.h
@@ -3,15 +3,6 @@
 
 #include "types.h"
 
-#define swap(x,y)\
-    do {\
-        typeof(x) _ = x;\
-        x = y;\
-        y = _;\
-    } while (0)
-
-#define arrsiz(x) (sizeof (x) / sizeof (x[0]))
-
 ul_t  rand_range(ul_t llim, ul_t ulim);
 ull_t gcd      (ull_t u, ull_t v);
 ull_t lcm      (ull_t u, ull_t v);
