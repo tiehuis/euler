1fdfa3757d5270fa9b6094736eb085d3b410a07a
Tiehuis <mti@protonmail.ch>
2014-09-26 16:54:55 +1200

    Added problem 22 sol

---
+++ src/022.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int name_value(char *s)
+{
+    int value = 0;
+    while (*s)
+        value += (toupper(*s++) - 'A') + 1;
+    return value;
+}
+
+int main(void)
+{
+    FILE *fd = fopen("res/022.data", "r");
+    char str[30];
+    long name_sum = 0;
+    size_t position = 1;
+
+    while (fscanf(fd, "%30s", str) != EOF)
+        name_sum += position++ * name_value(str);
+
+    printf("%ld\n", name_sum);
+    return 0;
+}
