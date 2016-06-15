53123e9e275dbfa7ca0cb96f1fc075a169e66075
tiehuis <marctiehuis@gmail.com>
2016-06-15 16:16:20 +1200

    Add problem 79 solution
    This was done by hand and was rather simple/fun.

---
+++ src/079.rs
@@ -0,0 +1,7 @@
+/// This can be performed by hand by just constructing the sequence as you go.
+/// Numbers which could lie in a given range can be put in a specific range
+/// until a further attempt is made which can narrow this down.
+
+fn main() {
+    println!("{}", 73_162_890);
+}
