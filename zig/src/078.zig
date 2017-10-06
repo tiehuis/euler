4579444e3ceba69ae1c08c7cb3ba537652846c61
Marc Tiehuis <marctiehuis@gmail.com>
2017-10-06 22:14:20 +1300

    Add solution 78

---
+++ zig/src/078.zig
@@ -0,0 +1,89 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+const ArrayList = std.ArrayList;
+
+fn Partition(comptime mod: u8) -> type { struct {
+    const Self = this;
+    const digit_count = {
+        var r: i64 = 1;
+        var i: u8 = 1;
+        while (i < mod) : (i += 1) {
+            r *= 10;
+        }
+        r
+    };
+
+    allocator: &Allocator,
+    partitions: ArrayList(i64),
+    n: usize,
+
+    pub fn init(allocator: &Allocator) -> Self {
+        Self {
+            .allocator = allocator,
+            .partitions = ArrayList(i64).init(allocator),
+            .n = 0,
+        }
+    }
+
+    pub fn deinit(p: &Self) {
+        p.partitions.deinit();
+    }
+
+    fn sign_m1(x: i64) -> i64 {
+        if (@mod(x, 2) == 0) i64(1) else -1
+    }
+
+    pub fn next(p: &Self) -> %i64 {
+        if (p.partitions.len == 0) {
+            %return p.partitions.appendSlice([]i64 { 1, 1, 2 });
+        }
+
+        const parts = p.partitions.toSliceConst();
+
+        if (p.n < 3) {
+            const result = parts[p.n];
+            p.n += 1;
+            return result;
+        }
+
+        var v: i64 = 0;
+        var k: i64 = 1;
+        var n = i64(parts.len);
+
+        while (n >= @divFloor(k * (3 * k - 1), 2)) {
+            v += sign_m1(k - 1) * parts[usize(n - @divFloor(k * (3 * k - 1), 2))];
+            k += 1;
+        }
+
+        k = -1;
+        while (n >= @divFloor(k * (3 * k - 1), 2)) {
+            v += sign_m1(k - 1) * parts[usize(n - @divFloor(k * (3 * k - 1), 2))];
+            k -= 1;
+        }
+
+        const final = @mod(v, digit_count);
+        %return p.partitions.append(final);
+        final
+    }
+}}
+
+fn run() -> u64 {
+    var allocator = &@import("../lib/c_allocator.zig").c_allocator;
+    var p = Partition(7).init(allocator);
+    defer p.deinit();
+
+    var n: u64 = 0;
+    while (true) : (n += 1) {
+        if (%%p.next() == 0) {
+            return n;
+        }
+    }
+}
+
+pub fn main() -> %void {
+    %%std.io.stdout.printf("{}\n", run());
+}
+
+test "078" {
+    std.debug.assert(run() == 55374)
+}
--- zig/src/build.zig
+++ zig/src/build.zig
@@ -3,19 +3,47 @@ const Mode = @import("builtin").Mode;
 const Builder = std.build.Builder;
 const TestStep = std.build.TestStep;
 
+const TestCase = struct {
+    filename: []const u8,
+    problem_type: ProblemType,
+};
+
+const ProblemType = enum {
+    Free,
+    LinkLibC,
+};
+
+fn Free(comptime filename: []const u8) -> TestCase {
+    TestCase {
+        .filename = filename,
+        .problem_type = ProblemType.Free,
+    }
+}
+
+fn LibC(comptime filename: []const u8) -> TestCase {
+    TestCase {
+        .filename = filename,
+        .problem_type = ProblemType.LinkLibC,
+    }
+}
+
 pub fn build(b: &Builder) {
-    const tests = [][]const u8 {
-        "001",
-        "002",
-        "116",
-        "117"
+    const tests = comptime []TestCase {
+        Free("001"),
+        Free("002"),
+        LibC("078"),
+        Free("116"),
+        Free("117"),
     };
 
     for ([]Mode { Mode.Debug, Mode.ReleaseSafe, Mode.ReleaseFast }) |mode| {
-        inline for (tests) |src_file| {
-            const all_tests = b.addTest(src_file ++ ".zig");
+        inline for (tests) |test_case| {
+            const all_tests = b.addTest(test_case.filename ++ ".zig");
             all_tests.setNamePrefix(b.fmt("{} ", @enumTagName(mode)));
             all_tests.setBuildMode(mode);
+            if (test_case.problem_type == ProblemType.LinkLibC) {
+                all_tests.linkSystemLibrary("c");
+            }
             b.default_step.dependOn(&all_tests.step);
         }
     }
